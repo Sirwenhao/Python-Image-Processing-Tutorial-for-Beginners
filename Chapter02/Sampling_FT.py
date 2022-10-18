8f8670cf3e8fc79bb02562fec3a745195a0190e8
Sirwenhao <1547102356@qq.com>
2022-10-18 23:45:15 +0800

    10/18

--- Chapter02/Sampling_FT.py
+++ Chapter02/Sampling_FT.py
@@ -69,19 +69,57 @@ def signaltonoise(a, axis=0, ddof=0):
 
 # 使用FFT计算DFT
 # 使用scipy.fftpack模块
-im = np.array(Image.open('Chapter01\Ch01images\\rhino.jpg'))
-SNR = signaltonoise(im, axis=None)
-print('SNR for the original image = ' + str(SNR))
-freq = fp.fft2(im)
-im1 = fp.ifft2(freq).real
-SNR = signaltonoise(im1, axis=None)
-print('SNR for the image obtained after reconstruction = ' + str(SNR))
-assert(np.allclose(im, im1))
-pylab.figure(figsize=(20, 10))
-pylab.subplot(121),pylab.imshow(im, cmap='gray'),pylab.axis('off')
-pylab.title('Original Image', size=10)
-pylab.subplot(122),pylab.imshow(im1, cmap='gray'),pylab.axis('off')
-pylab.title('Image obtained after reconstruction', size=10)
+# im = np.array(Image.open('Chapter01\Ch01images\\rhino.jpg'))
+# SNR = signaltonoise(im, axis=None)
+# print('SNR for the original image = ' + str(SNR))
+# freq = fp.fft2(im)
+# im1 = fp.ifft2(freq).real
+# SNR = signaltonoise(im1, axis=None)
+# print('SNR for the image obtained after reconstruction = ' + str(SNR))
+# assert(np.allclose(im, im1))
+# pylab.figure(figsize=(20, 10))
+# pylab.subplot(121),pylab.imshow(im, cmap='gray'),pylab.axis('off')
+# pylab.title('Original Image', size=10)
+# pylab.subplot(122),pylab.imshow(im1, cmap='gray'),pylab.axis('off')
+# pylab.title('Image obtained after reconstruction', size=10)
+# pylab.show()
+# freq2 = fp.fftshift(freq)
+# pylab.figure(figsize=(10,10)),pylab.imshow((20*np.log10(0.1+freq2)).astype(int)), pylab.show()
+
+# numpy的FFT模块
+import numpy.fft as fp
+im1 = rgb2gray(imread('Chapter01\Ch01images\house.jpg'))
+pylab.figure(figsize=(12, 10))
+freq1 = fp.fft2(im1)
+im1_ = fp.ifft2(freq1).real
+pylab.subplot(2,2,1),pylab.imshow(im1, cmap='gray'),pylab.title('Original Image', size=10)
+pylab.subplot(2,2,2),pylab.imshow(20*np.log10(0.01+np.abs(fp.fftshift(freq1))),cmap='gray')
+pylab.title('FFT Spectrum Magntitude',size=10)
+pylab.subplot(2,2,3),pylab.imshow(np.angle(fp.fftshift(freq1)),cmap='gray')
+pylab.title('FFT Phase',size=10)
+pylab.subplot(2,2,4),pylab.imshow(np.clip(im1_,0,255),cmap='gray')
+pylab.title('Reconstructed Image', size=10)
 pylab.show()
-freq2 = fp.fftshift(freq)
-pylab.figure(figsize=(10,10)),pylab.imshow((20*np.log10(0.1+freq2)).astype(int)), pylab.show()
+
+# import numpy.fft as fp
+im2 = rgb2gray(imread('Chapter01\Ch01images\house2.jpg'))
+pylab.figure(figsize=(12, 10))
+freq2 = fp.fft2(im2)
+im2_ = fp.ifft2(freq2).real
+pylab.subplot(2,2,1),pylab.imshow(im2, cmap='gray'),pylab.title('Original Image', size=10)
+pylab.subplot(2,2,2),pylab.imshow(20*np.log10(0.01+np.abs(fp.fftshift(freq2))),cmap='gray')
+pylab.title('FFT Spectrum Magntitude',size=10)
+pylab.subplot(2,2,3),pylab.imshow(np.angle(fp.fftshift(freq2)),cmap='gray')
+pylab.title('FFT Phase',size=10)
+pylab.subplot(2,2,4),pylab.imshow(np.clip(im2_,0,255),cmap='gray')
+pylab.title('Reconstructed Image', size=10)
+pylab.show()
+
+pylab.figure(figsize=(20, 15))
+im1_ = fp.ifft2(np.vectorize(complex)(freq1.real, freq2.imag)).real
+im2_ = fp.ifft2(np.vectorize(complex)(freq2.real, freq1.imag)).real
+pylab.subplot(221),pylab.imshow(np.clip(im1_,0,255),cmap='gray')
+pylab.title('Reconstructed Image (Re(F1) + Im(F2))',size=10)
+pylab.subplot(212).pylab.inshow(np.clip(im2_,0,255),cmap='gray')
+pylab.title('Reconstructed Image (Re(F2) + Im(F1))', size=10)
+pylab.show()
