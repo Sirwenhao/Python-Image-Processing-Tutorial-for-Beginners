38726f7949995213b7cf22fb2d84d6159f94c9be
Sirwenhao <1547102356@qq.com>
2022-10-25 23:57:37 +0800

    10/25

---
+++ Chapter04/Chapter04.py
@@ -0,0 +1,32 @@
+import numpy as np
+from skimage import data, img_as_float, img_as_ubyte,exposure, io, color
+from skimage.io import imread
+from skimage.exposure import cumulative_distribution
+from skimage.restoration import denoise_bilateral, denoise_nl_means, estimate_sigma
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+from skimage.util import random_noise
+from skimage.color import rgb2gray
+from PIL import Image, ImageEnhance, ImageFilter
+from scipy import ndimage, misc
+import matplotlib.pylab as pylab
+
+# 实现输入图像的颜色通道直方图
+def plot_image(image, title=''):
+    pylab.title(title, size=10), pylab.imshow(image)
+    pylab.axis('off')
+
+def plot_hist(r, g, b, title=''):
+    r, g, b = img_as_ubyte(r), img_as_ubyte(g), img_as_ubyte(b)
+    pylab.hist(np.array(r).ravel(), bins=256, range=(0, 256), color='r', alpha=0.5)
+    pylab.hist(np.array(g).ravel(), bins=256, range=(0, 256), color='g', alpha=0.5)
+    pylab.hist(np.array(b).ravel(), bins=256, range=(0, 256), color='b', alpha=0.5)
+    pylab.xlabel('pixel value', size=10), pylab.ylabel('frequency', size=10)
+    pylab.title(title, size=10)
+
+im = Image.open('Chapter01\Ch01images\parrot.jpg')
+im_r, im_g, im_b = im.split()
+pylab.style.use('ggplot')
+pylab.figure(figsize=(15, 5))
+pylab.subplot(121), plot_image(im, 'original image')
+pylab.subplot(122), plot_hist(im_r, im_g, im_b, 'historam for RGB channels')
+pylab.show()
