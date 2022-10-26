117e74b93df379318870e32bbc988a03aab52543
Sirwenhao <1547102356@qq.com>
2022-10-26 13:08:17 +0800

    10/26

--- Chapter04/Chapter04.py
+++ Chapter04/Chapter04.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageEnhance, ImageFilter
 from scipy import ndimage, misc
 import matplotlib.pylab as pylab
 
-# 实现输入图像的颜色通道直方图
+# # 实现输入图像的颜色通道直方图
 def plot_image(image, title=''):
     pylab.title(title, size=10), pylab.imshow(image)
     pylab.axis('off')
@@ -23,21 +23,43 @@ def plot_hist(r, g, b, title=''):
     pylab.xlabel('pixel value', size=10), pylab.ylabel('frequency', size=10)
     pylab.title(title, size=10)
 
-im = Image.open('Chapter01\Ch01images\parrot.jpg')
-im_r, im_g, im_b = im.split()
+# im = Image.open('Chapter01\Ch01images\parrot.jpg')
+# im_r, im_g, im_b = im.split()
+# pylab.style.use('ggplot')
+# pylab.figure(figsize=(15, 5))
+# pylab.subplot(121), plot_image(im, 'original image')
+# pylab.subplot(122), plot_hist(im_r, im_g, im_b, 'historam for RGB channels')
+# pylab.show()
+
+
+# # 使用PIL的point()函数进行对数变换
+# im = im.point(lambda i: 255*np.log(1+i/255))
+# im_r, im_g, im_b = im.split()
+# pylab.style.use('ggplot')
+# pylab.figure(figsize=(15, 5))
+# pylab.subplot(121), plot_image(im, 'image after log transform')
+# pylab.subplot(122), plot_hist(im_r, im_g, im_b, 'histogram of RGB channels log transform')
+# pylab.show()
+
+
+# 幂律变换
+im = img_as_float(imread('Chapter01\Ch01images\earthfromsky.jpg'))
+gamma = 5
+im1 = im**gamma
 pylab.style.use('ggplot')
 pylab.figure(figsize=(15, 5))
-pylab.subplot(121), plot_image(im, 'original image')
-pylab.subplot(122), plot_hist(im_r, im_g, im_b, 'historam for RGB channels')
+pylab.subplot(121), plot_hist(im[..., 0], im[..., 1], im[..., 2], 'histogram for RGB channels (iutput)')
+pylab.subplot(122), plot_hist(im1[..., 0], im1[..., 1], im1[..., 2], 'histogram for RGB channels (output)')
 pylab.show()
 
-
-# 使用PIL的point()函数进行对数变换
-im = im.point(lambda i: 255*np.log(1+i/255))
+# PIL作为点操作
+im = Image.open('Chapter01\Ch01images\cheetah.jpg')
 im_r, im_g, im_b = im.split()
 pylab.style.use('ggplot')
 pylab.figure(figsize=(15, 5))
-pylab.subplot(121), plot_image(im, 'image after log transform')
-pylab.subplot(122), plot_hist(im_r, im_g, im_b, 'histogram of RGB channels log transform')
+pylab.subplot(121)
+plot_image(im)
+pylab.subplot(122)
+plot_hist(im_r, im_g, im_b)
 pylab.show()
 
